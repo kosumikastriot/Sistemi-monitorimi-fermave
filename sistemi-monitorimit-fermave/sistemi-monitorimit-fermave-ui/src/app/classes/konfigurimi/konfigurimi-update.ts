f876c31d4d3bf1560e3a02229b127bf59bd61b8a
Edony <eg36577@ubt-uni.net>
2020-05-25 16:29:10 +0200

    Cakto Konfigurimin

---
+++ sistemi-monitorimit-fermave/sistemi-monitorimit-fermave-ui/src/app/classes/konfigurimi/konfigurimi-update.ts
@@ -0,0 +1,11 @@
+export class KonfigurimiUpdate {
+  id: Number;
+  vleraMin: Number;
+  vleraMax: Number;
+
+  constructor(id: Number, vleraMin: Number, vleraMax: Number) {
+    this.id = id;
+    this.vleraMin = vleraMin;
+    this.vleraMax = vleraMax;
+  }
+}
--- sistemi-monitorimit-fermave/sistemi-monitorimit-fermave-ui/src/app/sistemi-monitorimit-fermave/konfigurimet/konfigurimet.module.ts
+++ sistemi-monitorimit-fermave/sistemi-monitorimit-fermave-ui/src/app/sistemi-monitorimit-fermave/konfigurimet/konfigurimet.module.ts
@@ -4,13 +4,17 @@ import { KonfigurimetComponent } from './konfigurimet.component';
 import {KonfigurimetRoutingModule} from './konfigurimet-routing.module';
 import {SharedModule} from '../../shared/shared.module';
 import {ChartModule} from 'angular2-chartjs';
+import {FormsModule} from '@angular/forms';
+import {ToastyModule} from 'ng2-toasty';
 
 @NgModule({
   imports: [
     CommonModule,
     KonfigurimetRoutingModule,
     SharedModule,
-    ChartModule
+    ChartModule,
+    FormsModule,
+    ToastyModule
   ],
   declarations: [KonfigurimetComponent]
 })
